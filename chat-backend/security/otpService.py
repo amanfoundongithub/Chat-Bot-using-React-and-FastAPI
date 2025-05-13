29a2b024c6e2bbe275c2c125dd6d932112811036
Aman24Raj <amanrajmathematics@gmail.com>
2025-05-13 16:44:17 +0530

    Security

--- chat-backend/security/otpService.py
+++ chat-backend/security/otpService.py
@@ -3,10 +3,6 @@ from pydantic import BaseModel
 otp_store = {}
 
 # Load SMTP credentials from environment
-SMTP_HOST = "smtp.gmail.com"
-SMTP_PORT = 587
-SMTP_USER = "billygates124356@gmail.com"
-SMTP_PASS = "fvly kegh myrn vroc"
 
 # Pydantic Methods
 class OTPRequest(BaseModel):
